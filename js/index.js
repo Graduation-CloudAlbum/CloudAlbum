49dcd625cb5fa12442e6611c0458ff36e0bcefcd
lan.huang@dxc.com <lan.huang@dxc.com>
2018-11-27 15:36:38 +0800

    popup

--- js/index.js
+++ js/index.js
@@ -1,15 +1,46 @@
-var navFriend1 = document.getElementById('navFriend1');
-var navFriend2 = document.getElementById('navFriend2');
-var navFriend3 = document.getElementById('navFriend3');
-function load_home1() {
-     document.getElementById("main").innerHTML = '<object type="text/html" data="html/personalData.html" width="100%" height="100%"></object>';
- }
-function load_home2() {
-     document.getElementById("main").innerHTML = '<object type="text/html" data="html/myAlbum.html" width="100%" height="100%"></object>';
- }
-function load_home3() {
-     document.getElementById("main").innerHTML = '<object type="text/html" data="html/myFriend.html" width="100%" height="100%"></object>';
- }
- function load_home4() {
-     document.getElementById("main").innerHTML = '<object type="text/html" data="html/recycleBin.html" width="100%" height="100%"></object>';
- }
+var uploadPhoto1 = document.getElementById('uploadPhoto1');
+var iconChacha1 = document.getElementById('icon-chacha1');
+var popLayer = document.getElementById('popLayer');//upload-photos-choose
+//点击上传照片
+uploadPhoto1.onclick=function(){
+	document.getElementById('upload-photos').style.display="block";
+	document.getElementById('popLayer').style.display="block";
+	document.getElementById('popLayer2').style.display="block";
+	document.getElementById('upload-photos-choose').style.display="block";
+}
+//关闭上传照片
+iconChacha1.onclick=function(){
+	document.getElementById('upload-photos').style.display="none";
+	document.getElementById('popLayer').style.display="none";
+}
+//关闭选择相册弹出框
+var iconChacha2 = document.getElementById('iconChacha2');
+iconChacha2.onclick=function(){
+	document.getElementById('upload-photos-choose').style.display="none";
+	document.getElementById('popLayer2').style.display="none";
+	document.getElementById('upload-photos').style.display="none";
+	document.getElementById('popLayer').style.display="none";
+}
+//上传照片下一步
+var selectButton = document.getElementById('select-button');
+selectButton.onclick=function(){
+	document.getElementById('upload-photos-choose').style.display="none";
+	document.getElementById('popLayer2').style.display="none";
+}
+//创建相册
+var uploadPhoto2 = document.getElementById('uploadPhoto2');//Create-Album
+uploadPhoto2.onclick=function(){
+	document.getElementById('popLayer2').style.display="block";
+	document.getElementById('Create-Album').style.display="block";
+}
+//关闭创建相册
+var iconChacha3 = document.getElementById('iconChacha3');
+iconChacha3.onclick=function(){
+	document.getElementById('popLayer2').style.display="none";
+	document.getElementById('Create-Album').style.display="none";
+}
+var CreateAlbumButton1 = document.getElementById('Create-Album-button1');
+CreateAlbumButton1.onclick=function(){
+	document.getElementById('popLayer2').style.display="none";
+	document.getElementById('Create-Album').style.display="none";
+}
